6f0e0ef07125d496482603e962ebe5f4ba6d4d47
Geo <geoncic@gmail.com>
2022-01-10 17:53:29 -0600

    Complete through Day 12

--- Day 12/main.py
+++ Day 12/main.py
@@ -6,14 +6,12 @@ def bfs():
         if path[-1] == 'end':
             all_paths.add(path)
             continue
-
         for cand in edges[path[-1]]:
             if not cand.islower() or cand not in path:
                 todo_bfs.append((*path, cand))
     return len(all_paths)
 
 def dfs(path: list[list[str]], visited: set[str], cave: [str]):
-
     if cave == 'end':
         solutions_dfs.append(path)
     else:
@@ -22,6 +20,20 @@ def dfs(path: list[list[str]], visited: set[str], cave: [str]):
         for next_cave in edges[cave]:
             if next_cave not in visited:
                 dfs(path + [next_cave], set(visited), next_cave)
+
+def dfs_2(path: list[list[str]], visited: set[str], cave: [str], twice: set[str]):
+    if cave == 'end':
+        solutions_dfs.append(path)
+    else:
+        if cave.islower():
+            if cave in visited and cave not in ['start', 'end']:
+                twice.add(cave)
+            visited.add(cave)
+        for next_cave in edges[cave]:
+            if next_cave not in visited:
+                dfs_2(path + [next_cave], set(visited), next_cave, set(twice))
+            elif next_cave not in ['start', 'end'] and len(twice) == 0:
+                dfs_2(path + [next_cave], set(visited), next_cave, set(twice))
 
 def read_file():
     with open('input.txt') as f:
@@ -40,8 +52,10 @@ if __name__ == "__main__":
     todo_bfs = [('start',)]
     todo_dfs = ['start']
     all_paths = set()
+    revisited = set()
     solutions_dfs = []
-    dfs(todo_dfs, set(), "start")
+    solutions_dfs_2 = []
+    dfs_2(todo_dfs, set(), "start", set())
 
     print(f'Part One: BFS Path Count is {bfs()}')
-    print(f'Part One: DFS Path Count is {len(solutions_dfs)}')
+    print(f'Part Two: DFS Path Count is {len(solutions_dfs)}')
