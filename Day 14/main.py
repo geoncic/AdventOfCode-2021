5d3a8ad3ef1a12b9ca2e1e499563007beeb948bf
Geo <geoncic@gmail.com>
2022-01-18 16:45:04 -0600

    Complete through Day 14, part one

--- Day 14/main.py
+++ Day 14/main.py
@@ -1,17 +1,37 @@
 from collections import defaultdict
+import pandas as pd
+
 
 def read_file():
-    with open('input.txt') as f:
+    with open('test.txt') as f:
         file = f.read().split('\n')
     return file
 
 
-def part_one():
-    pass
+def part_one(polymer, inst, steps):
+    for x in range(steps):
+        print(x)
+        polymer = polymer_insert(polymer, inst)
+    poly_count = pd.Series(list(polymer)).value_counts()
+    max_count = poly_count.max()
+    min_count = poly_count.min()
+    return max_count-min_count
 
 
 def part_two():
     pass
+
+
+def polymer_insert(polymer, inst):
+    chain_build = [polymer[0]]
+    for i in range(len(polymer) - 1):
+        segment = polymer[i] + polymer[i + 1]
+        if segment in inst:
+            chain_build.append(inst[segment] + polymer[i + 1])
+        else:
+            print(f'Segment {segment} is not in instruction dictionary')
+    new_polymer = ''.join(chain_build)
+    return new_polymer
 
 
 if __name__ == "__main__":
@@ -22,5 +42,8 @@ if __name__ == "__main__":
         if '->' in line:
             pair, ins = line.split(' -> ')
             instructions[pair] = ins
-    print(instructions)
-    print(chain)
+
+    part_one_sol = part_one(chain, instructions, 20)
+
+    print(f'Part One: Solution is {part_one_sol}')
+    
