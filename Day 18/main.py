2fbd72dd715067c32392b26200463d6227dd7e08
geoncic <geoncic@gmail.com>
2022-01-23 00:25:40 -0600

    // Day 18 clean up

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,72 +1,41 @@
-
 import ast
 from collections.abc import Sequence
-from collections import defaultdict
 from itertools import chain, count
 
 
-def reduce(snailfish: list[list]) -> list:
-    pass
+def calculate(snailfish: list[list], sig: str, type: str, operation: str) -> dict:
+    ret = {}
+    split_dict(snailfish, sig, 'list', operation, ret)
+    return ret
 
-def check(snailfish: list[list], sig: str, type: str, operation: str) -> bool:
-    # print(snailfish)
-    # levels = depth(snailfish)
-    # print(levels)
-    split_dict(snailfish, sig, 'list', operation)
-
-
-def split_dict(seq: list, sig: str, type: str, operation: str):
+def split_dict(seq: list, sig: str, type: str, operation: str, map: dict):
     operation = 'None'
-    # print(len(seq))
     left = seq[0]
     left_sig = sig + '0'
-    # print(f'Left Side: {left} with Sig: {left_sig}')
     right = seq[1]
     right_sig = sig + '1'
-    # print(f'Right Side: {right} with Sig: {right_sig}')
-    # if levels == 0:
-    #     print(f'Last level -> Left: {left}, Right -> {right}: Level: {levels}')
-    # levels = depth(seq)
-
-    # fish_dict[left_sig] = [str(left).replace(" ", ""), type, operation]
-    # fish_dict[right_sig] = [str(right).replace(" ", ""), type, operation]
-
-    fish_dict[left_sig] = [left, type, operation]
-    fish_dict[right_sig] = [right, type, operation]
-
-
-    # fish_dict[str(left).replace(" ", "")] = [left_sig, type, operation]
-    # fish_dict[str(right).replace(" ", "")] = [right_sig, type, operation]
+    map[left_sig] = [left, type, operation]
+    map[right_sig] = [right, type, operation]
 
     if len(left_sig) > 3 and isinstance(left, list):
-        # print(f'Left Explodes! {left} {left_sig} {type}')
         operation = 'Explode'
-
-        fish_dict[left_sig][2] = 'Explode'
-        # fish_dict[str(left).replace(" ", "")][2] = 'Explode'
-        # print(fish_dict[str(left).replace(" ", "")])
+        map[left_sig][2] = operation
     if len(right_sig) > 3 and isinstance(right, list):
-        # print(f'Right Explodes! {right} {right_sig} {type}')
         operation = 'Explode'
-        fish_dict[right_sig][2] = 'Explode'
-        # fish_dict[str(left).replace(" ", "")][2] = 'Explode'
-        # print(fish_dict[str(right).replace(" ", "")])
+        map[right_sig][2] = operation
     if isinstance(left, list):
-        # print(f'Checking Further: {left} {left_sig} {type}')
-        leftcheck = check(left, left_sig, 'list', operation)
+        # check(left, left_sig, 'list', operation)
+        split_dict(left, left_sig, 'list', operation, map)
+
     else:
         type = 'int'
-        fish_dict[left_sig][1] = type
-        # fish_dict[str(left).replace(" ", "")][1] = type
-        # print(f'Dead End for Left: {left}, Level: {left_sig}, Type: {type}')
+        map[left_sig][1] = type
     if isinstance(right, list):
-        # print(f'Checking Further: {right} {right_sig} {type}')
-        rightcheck = check(right, right_sig, 'list', operation)
+        # rightcheck = calculate(right, right_sig, 'list', operation)
+        split_dict(right, right_sig, 'list', operation, map)
     else:
         type = 'int'
-        fish_dict[right_sig][1] = type
-        # fish_dict[str(right).replace(" ", "")][1] = type
-        # print(f'Dead End for Right: {right}, Level: {right_sig}, Type: {type}')
+        map[right_sig][1] = type
 
 def num_dict(seq: list) -> dict:
     pass
@@ -86,12 +55,6 @@ def reduce(sf_dict: dict) -> dict:
         for i in range(len(key)):
             pre_key += key[i]
             list_copy = sf_dict[pre_key][0]
-            # print('\n')
-            # print(f'Key: {key}')
-            # print(f'Pre Key: {pre_key}')
-            # print(f'Dict Value: {sf_dict[pre_key][0]}')
-            # print(f'Copy: {list_copy}')
-
             zero_key = key[len(pre_key):]
             # print(f' Zero Key: {zero_key}')
             # if len(zero_key) == 3:
@@ -103,16 +66,10 @@ def reduce(sf_dict: dict) -> dict:
 
     def _sum(key, value):
         pre_key = ''
-        print(f'Sum key is {key}')
-        print(f'Sum Value is {value}')
         for i in range(len(key)):
-            print(f'i is {i}')
-
             pre_key += key[i]
             list_copy = sf_dict[pre_key][0]
-
-            zero_key = key[len(pre_key):]
-            print(f' Sum Key: {zero_key}')
+            sum_key = key[len(pre_key):]
 
             """Take advantage of the mutable property of lists, and only
             update one...is that what's actually going on here???? 
@@ -124,51 +81,53 @@ def reduce(sf_dict: dict) -> dict:
             # if len(zero_key) == 2:
             #     print(f'Adding to {zero_key}')
             #     list_copy[int(zero_key[0])][int(zero_key[1])] += value
-            if len(zero_key) == 1:
-                print(f'Adding to {zero_key}')
-                list_copy[int(zero_key[0])] += value
+
+            if len(sum_key) == 1:
+                list_copy[int(sum_key[0])] += value
 
 
+
+    # Getting so close, not doing correctly if adjacent number is inside a nested list
 
     for key, value in list(sf_dict.items()):
-        # if len(value[0]) > 3:
-        #     print(key)
-        #     print(value)
+        print(f'Key is: {key}')
         if value[2] == "Explode":
-
+            side = key[-1]
             sig_trim = key[:-1]
-            left_check_sig_flip = sig_trim[-1].replace('1', '2').replace('0', '1').replace('2', '0')
-            left_check_sig = sig_trim[:-1] + left_check_sig_flip
-            right_check_sig_flip = key[-1].replace('1', '2').replace('0', '1').replace('2', '0')
+            if side:
+                print(f'Explode list is right side')
+                left_check_sig_flip = key[-1].replace('1', '2').replace('0', '1').replace('2', '0')
+                left_check_sig = key[:-1] + left_check_sig_flip
+                right_check_sig_flip = sig_trim[-1].replace('1', '2').replace('0', '1').replace('2', '0')
+                print(f'Left Check Sig Flip: {left_check_sig_flip}')
+                right_check_sig = sig_trim[:-1] + right_check_sig_flip
+                print(f'Right Check Sig: {right_check_sig}')
+                
+            else:
+                print(f'Explode list is left side')
+                print(f'Sig trim: {sig_trim}')
+                left_check_sig_flip = sig_trim[-1].replace('1', '2').replace('0', '1').replace('2', '0')
+                print(f'Left Check Sig Flip: {left_check_sig_flip}')
+                left_check_sig = sig_trim[:-1] + left_check_sig_flip
+                print(f'Left Check Sig: {left_check_sig}')
+                right_check_sig_flip = key[-1].replace('1', '2').replace('0', '1').replace('2', '0')
+                print(f'Right Check Sig Flip: {right_check_sig_flip}')
+                right_check_sig = key[:-1] + right_check_sig_flip
+                print(f'Right Check Sig: {right_check_sig}')
+                # left_key = find_sig_key(sf_dict, left_check_sig)
+                # right_key = find_sig_key(sf_dict, right_check_sig)
 
 
-            right_check_sig = key[:-1] + right_check_sig_flip
-            # left_key = find_sig_key(sf_dict, left_check_sig)
-            # right_key = find_sig_key(sf_dict, right_check_sig)
-
-            # print(key)
-            # print(f'Max key {max(key)}')
             if max(key) != "0":
-                # print(sf_dict[left_check_sig])
+                print(f'Left Check Sig: {left_check_sig}')
+                print(sf_dict[left_check_sig])
                 if sf_dict[left_check_sig][1] == 'int':
-
-                    # print(f'Value {value[0]}')
-                    # print(f'Left {value[0][0]}')
-                    # print(f'Modify {sf_dict[left_check_sig]}')
-                    # sf_dict[left_check_sig][0] +=  value[0][0]
-                    # print(f'Value added to Left: {sf_dict[left_check_sig]}')
                     _sum(left_check_sig, value[0][0])
-
             else:
                 print(f'Value: {value[0]} with key {key} is left most {value[1]}')
+
             if min(key) != "1":
-                # print(sf_dict[right_check_sig])
                 if sf_dict[right_check_sig][1] == 'int':
-                    # print(f'Value {value[0]}')
-                    # print(f'Right {value[0][1]}')
-                    # print(f'Modify {sf_dict[right_check_sig]}')
-                    # sf_dict[right_check_sig][0] += value[0][1]
-                    # print(f'Value added to Right: {sf_dict[right_check_sig]}')
                     _sum(right_check_sig, value[0][1])
             else:
                 print(f'Value: {value[0]} with key {key} is right most {value[1]}')
@@ -178,92 +137,23 @@ def reduce(sf_dict: dict) -> dict:
             sf_dict.pop(key + '1', None)
 
             _zero(key)
-
-
-            # pre_key = ''
-
-
-            # for i in range(len(key)):
-            #     # list_copy = []
-            #
-            #     pre_key += key[i]
-            #
-            #     list_copy = sf_dict[pre_key][0]
-            #
-            #     # try:
-            #     #     list_copy = sf_dict[pre_key][0]
-            #     # except AttributeError:
-            #     #     print(f'Attribute Error with {sf_dict[pre_key][0]}')
-            #     #     list_copy = [sf_dict[pre_key][0]]
-            #     print('\n')
-            #     print(f'Key: {key}')
-            #     print(f'Pre Key: {pre_key}')
-            #     print(f'Dict Value: {sf_dict[pre_key][0]}')
-            #     print(f'Copy: {list_copy}')
-            #
-            #     zero_key = key[len(pre_key):]
-            #
-            #
-            #     print(f' Zero Key: {zero_key}')
-            #
-            #     if len(zero_key) == 3:
-            #         list_copy[int(zero_key[0])][int(zero_key[1])][int(zero_key[2])] = 0
-            #     if len(zero_key) == 2:
-            #         list_copy[int(zero_key[0])][int(zero_key[1])] = 0
-            #     if len(zero_key) == 1:
-            #         list_copy[int(zero_key[0])] = 0
-
-
-
-                # try:
-                #     print(f'Key 110: {list_copy[1][1][0]}')
-                # except TypeError:
-                #     print('Tis an Int')
-                #     print(list_copy)
-                # try:
-                #     print(f'Key 10: {list_copy[1][0]}')
-                # except TypeError:
-                #     print('Tis an Int')
-                #     print(list_copy)
-                # try:
-                #     print(f'Key 0: {list_copy[0]}')
-                # except TypeError:
-                #     print('Tis an Int')
-                #     print(list_copy)
-
-            # sig_trim = value[0][:-1]
-            # left_check_sig_flip = sig_trim[-1].replace('1', '2').replace('0', '1').replace('2', '0')
-            # left_check_sig = sig_trim[:-1] + left_check_sig_flip
-            # right_check_sig_flip = value[0][-1].replace('1', '2').replace('0', '1').replace('2', '0')
-            # right_check_sig = value[0][:-1] + right_check_sig_flip
-            # left_key = find_sig_key(sf_dict, left_check_sig)
-            # right_key = find_sig_key(sf_dict, right_check_sig)
-            # print(key)
-            # print(value[0])
-            # print(left_key)
-            # print(left_check_sig)
-            # print(right_key)
-            # print(right_check_sig)
-
             print(sf_dict)
 
-            new_sfnumber = []
-            new_sfnumber.append(sf_dict['0'][0])
-            new_sfnumber.append(sf_dict['1'][0])
+    new_sfnumber = []
+    new_sfnumber.append(sf_dict['0'][0])
+    new_sfnumber.append(sf_dict['1'][0])
 
-            print('\n')
-            print(new_sfnumber)
+    print('\n')
+    print(new_sfnumber)
 
 def find_sig_key(input_dict: dict, value: str):
     return {k for k, v in input_dict.items() if k == value}
-
 
 def add_fish(a: list, b: list) -> list:
     comb_fish = []
     comb_fish.append(a)
     comb_fish.append(b)
     return comb_fish
-
 
 def read_file():
     with open('input.txt') as f:
@@ -279,11 +169,13 @@ if __name__ == "__main__":
     # comb_fish = add_fish(data[0], data[1])
     # print(comb_fish)
     # check(comb_fish)
-    print(data[0])
+    # print(data[1])
     # fish_dict = defaultdict()
-    fish_dict = {}
-    check(data[0], '', 'list', 'None')
-    print(fish_dict)
+    # map = {}
+    print(depth(data[0]))
+    print(data[0])
+    fish_dict = calculate(data[0], '', 'list', 'None')
+    # print(fish_dict)
     reduce(fish_dict)
     # new_fish = add_fish(data[0], data[1])
 
