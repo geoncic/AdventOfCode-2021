f393923df5d27f621c95507055d3b91c91ea8c10
geoncic <geoncic@gmail.com>
2022-01-30 15:16:54 -0600

    Complete through Day 20, part one

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,35 +1,61 @@
 import argparse
-import collections
 import os.path
+import numpy as np
 from typing import Generator
 
 
-INPUT_TXT = os.path.join(os.path.dirname(__file__), 'test.txt')
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def part_one(a: str, img: list) -> int:
+    steps = 2
+    final_size = ((len(img)+2*steps), (len(img[0])+2*steps))
+    pcount = 0
+
+    for s in range(steps):
+        img = day_twenty(a, img)
+
+    while len(img) > final_size[0]:
+        img = img[1:-1, 1:-1]
+    for line in img:
+        for p in line:
+            if p == '#':
+                pcount += 1
+
+    return pcount
 
 
 def day_twenty(algo: str, image: list) -> ...:
-    print(algo)
-    print(image)
-    coords = collections.defaultdict(lambda: x)
+    image = expand_image(image)
+    new_image = image.copy()
+
     for y, line in enumerate(image):
         for x, pixel in enumerate(line):
-            coords[(x, y)]  = pixel
+            pixel_sig = []
+            for pt in adjacent(x, y):
+                ix, iy = pt
+                try:
+                    pixel_sig.append(image[iy][ix])
+                except IndexError:
+                    pixel_sig.append('.')
+            alg_index = pix_string_to_int(pixel_sig)
+            new_image[y][x] = algo[alg_index]
 
-            # if pixel == '#':
-            #     coords[(x, y)] = 1
-            # else:
-            #     coords[(x, y)] = 0
-    print(coords)
+    return new_image
 
-    new_image = collections.defaultdict(lambda: x)
 
-    for (x, y), n in coords.items():
-        todo = []
-        print(f'Evaluating Point ({x}, {y})')
-        for pt in adjacent(x, y):
-            todo.append(coords.get(pt, '.'))
+def pix_string_to_int(plist: list) -> int:
+    bin_str = ''
+    for p in plist:
+        if p == '.':
+            bin_str += '0'
+        else:
+            bin_str += '1'
+    return int(bin_str, 2)
 
-        print(todo)
+
+def expand_image(image: list):
+    return np.pad(image, pad_width=4, mode='constant', constant_values='.')
 
 
 def adjacent(x: int, y: int) -> Generator[tuple[int, int], None, None]:
@@ -37,10 +63,12 @@ def adjacent(x: int, y: int) -> Generator[tuple[int, int], None, None]:
     yield x, y - 1
     yield x + 1, y - 1
     yield x - 1, y
+    yield x, y
     yield x + 1, y
     yield x - 1, y + 1
     yield x, y + 1
     yield x + 1, y + 1
+
 
 def read_file():
     parser = argparse.ArgumentParser()
@@ -61,7 +89,7 @@ def read_file():
 
 def main():
     algo, image = read_file()
-    day_twenty(algo, image)
+    print(f'Part One: {part_one(algo, image)}')
 
 
 if __name__ == "__main__":
